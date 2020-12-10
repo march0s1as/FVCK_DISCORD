d25dbe8d51c64dc05dd99bb8048174c926f548d9
blackcrw <37225059+blkzy@users.noreply.github.com>
2020-12-09 23:48:59 -0300

    Create banner.py

---
+++ lib/banner.py
@@ -0,0 +1,18 @@
+def banner_centauro():
+	text = white + """
+ <=======]}======
+    --.   /|         Script produzido por: march0s1as the god.
+   _\"/_.'/          Por favor, não faça o mal uso do mesmo.
+ .'._._,.'
+ :/ \{}/
+(L  /--',----._
+    |          \\
+   : /-\ .'-'\ / |
+    \\, ||    \|
+     \/ ||    ||                     
+
+Siga-nos no servidor do discord! https://discord.gg/v5d3PZ9
+Follow us in our discord server! https://discord.gg/v5d3PZ9
+        """
+
+	return text
